b82dbc16bf9b0e8142fa1f53fecfcdea4d1ec6c7
Dixon <zdixon91.zd@gmail.com>
2022-03-17 14:35:17 -0500

    formatted task name and type

--- assets/script.js
+++ assets/script.js
@@ -3,11 +3,24 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 
 var createTaskHandler = function(event) {
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
     event.preventDefault();
 
+    //create list item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
+
+    //create div to hold task info and add to list item
+    var taskInfoEl = document.createElement("div");
+    listItemEl.className = "task-item";
+
+    //add HTML contetn to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+
+    listItemEl.appendChild(taskInfoEl);
+
+    //add entire list item to list
     tasksToDoEl.appendChild(listItemEl);
 };
 
