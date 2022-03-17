18706e339f85787988148bb9b972c077c7704869
Dixon <zdixon91.zd@gmail.com>
2022-03-17 13:37:57 -0500

    created form

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,14 @@
-var buttonE1 = document.querySelector("#save-task");
-console.log(buttonE1);
-
-buttonE1. addEventListener("click", function() {
-    alert(none);
-})
-
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function() {
+
+var createTaskHandler = function(event) {
+    event.preventDefault();
+
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
-}
+};
 
-buttonE1.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
